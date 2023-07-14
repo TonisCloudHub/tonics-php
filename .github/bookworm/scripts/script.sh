0a6215b2c53cfa1bc0dc0c24c8eba8f7672dd47e
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2023-07-14 05:14:35 +0100

    Update script.sh

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -9,7 +9,7 @@ lxc exec tonics-php -- bash -c "apt update -y && apt upgrade -y"
 lxc exec tonics-php -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y php php8.2-fpm php8.2-mysql php8.2-mbstring php8.2-readline php8.2-gd  php8.2-gmp php8.2-bcmath  php8.2-zip php8.2-curl php8.2-intl php8.2-apcu"
 
 # Clean Debian Cache
-lxc exec tonics-php -- apt clean
+lxc exec tonics-php -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # Version
 Version=$(lxc exec tonics-php -- php -v | head -n 1 | awk '{print $2}' | cut -d '-' -f 1)
