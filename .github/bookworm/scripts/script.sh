3036c1355a6dbd0e1f0272ac6ba5901f8a60a974
Olayemi.Faruq <olayemi.faruq@techsigns.de>
2024-05-10 16:12:35 +0100

    ynl update

--- .github/bookworm/scripts/script.sh
+++ .github/bookworm/scripts/script.sh
@@ -11,6 +11,17 @@ sudo incus launch images:debian/bookworm/amd64 tonics-php
 # Dependencies
 sudo incus exec tonics-php -- bash -c "apt update -y && apt upgrade -y"
 
+if (( $(echo "$PHP_VERSION > 8.2" | bc -l) )); then
+
+  # Add Ondrej's repo source and signing key along with dependencies
+  sudo incus exec tonics-php -- bash -c "apt install apt-transport-https"
+  sudo incus exec tonics-php -- bash -c  "curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg"
+  sudo incus exec tonics-php -- bash -c <<EOS
+  echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+EOS
+
+fi
+
 sudo incus exec tonics-php -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y php php$PHP_VERSION-fpm php$PHP_VERSION-mysql php$PHP_VERSION-mbstring php$PHP_VERSION-readline php$PHP_VERSION-gd  php$PHP_VERSION-gmp php$PHP_VERSION-bcmath  php$PHP_VERSION-zip php$PHP_VERSION-curl php$PHP_VERSION-intl php$PHP_VERSION-apcu"
 
 # Clean Debian Cache
